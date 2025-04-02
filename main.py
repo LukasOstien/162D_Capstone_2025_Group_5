f8b494ebb9f4f571dcb912072355f242b469b022
anliliu1 <anliaa@g.ucla.edu>
2025-04-01 17:04:55 -0700

    why red lines

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 print("Hello, my name is Lukas.")
 print("Hello, my name is Anthony")
 print("Hello, my name is Anli")
-<<<<<<< HEAD
 print("hamburger bun")
 print("peepeepoopoo")
-=======
 print("hotdog bun")
->>>>>>> 5915b23f45e09ff8e5cc66f713c3dfdbd7302e41
+ 
