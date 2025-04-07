93c3ac5c1e6ad4db1cefe78174f812c94604ae15
Lukas Ostien <lukas.ostien@gmail.com>
2025-04-07 08:18:40 -0700

    Delete Code/Lab_9_Code/src/Lab_10_code directory

--- Code/Lab_9_Code/src/Lab_10_code/src/SimulinkGenerate.m
+++
@@ -1,5 +0,0 @@
-% -------------------------------------------------------------------
-% Run this sketch to set the matlab workspace (directory) to scr folder
-% so the generated code will be in the right place
-open("ControlLoop.slx");
-open("StateflowBlock.slx");
